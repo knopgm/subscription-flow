fe5ac71c4ba118c09e68463983044346aea8aa70
Gabriela Knop Marques <knop.gm@gmail.com>
2021-06-11 09:49:41 +0200

    Create subscriptions plans form and logic parameters

--- src/pages/Plans/Plans.jsx
+++ src/pages/Plans/Plans.jsx
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
+import { useForm } from "react-hook-form";
 
 import { Container } from "../../components/Container";
 import { useSubscriptionFlow } from "../../utilities/subscription-flow";
-import { Card } from "./components/Card";
+import { SubscriptionForm } from "./components/SubscriptionForm";
 
 //import styles from "./style.scss";
 
 export function Plans() {
   const history = useHistory();
-  const { subscriptionFlowState, setPlan } = useSubscriptionFlow();
-
-  console.log({ subscriptionFlowState });
+  const {
+    subscriptionFlowState,
+    setPlan,
+    setPricingOptions,
+  } = useSubscriptionFlow();
+  const { register, handleSubmit, watch } = useForm({
+    defaultValues: {
+      amountGb: subscriptionFlowState.pricingOptions.amountGb,
+      upfrontPayment: subscriptionFlowState.pricingOptions.upfrontPayment,
+    },
+  });
 
   const [plans, setPlans] = useState([]);
 
@@ -23,23 +32,81 @@ export function Plans() {
       });
   }, []);
 
-  const handleSubmit = (data) => {
-    setPlan(data);
+  const { amountGb, upfrontPayment } = watch();
+
+  const [selectedPlan, setSelectedPlan] = useState({
+    durationMonths: 12,
+    priceUsdPerGb: 2,
+    id: 2,
+  });
+
+  const handlePlanSelect = (data) => {
+    setSelectedPlan(data);
+  };
+
+  const onSubmit = (data) => {
+    const plan = {
+      durationMonths: selectedPlan.durationMonths,
+      priceUsdPerGb: selectedPlan.priceUsdPerGb,
+    };
+    const pricingOptions = {
+      amountGb: data.amountGb,
+      upfrontPayment: data.upfrontPayment,
+    };
+    setPlan(plan);
+    setPricingOptions(pricingOptions);
+
     history.push("/payment");
   };
 
+  const subtotal =
+    amountGb * selectedPlan?.priceUsdPerGb * selectedPlan?.durationMonths;
+
+  const discount = upfrontPayment ? subtotal * 0.1 : 0;
+  const total = subtotal - discount;
+
   return (
     <Container>
-      {plans.map((plan) => {
+      <header>
+        <h1>Welcome to Cloudy</h1>
+        <h2>Your Cloud Storage Service</h2>
+      </header>
+
+      {plans.map((plan, index) => {
         return (
-          <Card
+          <SubscriptionForm
+            selected={selectedPlan?.id === index}
             durationMonths={plan.duration_months}
             priceUsdPerGb={plan.price_usd_per_gb}
-            key={plan.duration_months}
-            onSubmit={handleSubmit}
+            key={index}
+            id={index}
+            onSelect={handlePlanSelect}
           />
         );
       })}
+
+      <div>
+        <div>Select the Amount of Gb:</div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <select {...register("amountGb")}>
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="50">50</option>
+          </select>
+
+          <label htmlFor="upfrontPayment">Upfront Payment</label>
+          <input
+            {...register("upfrontPayment", { required: false })}
+            type="checkbox"
+          />
+
+          <div>Subtotal: ${subtotal}</div>
+          <div>Discount: ${discount}</div>
+          <div>Total: ${total}</div>
+
+          <input type="submit" />
+        </form>
+      </div>
     </Container>
   );
 }
