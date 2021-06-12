06579cf8aee741abe7c40d242b70e97f23885eb9
Gabriela Knop Marques <knop.gm@gmail.com>
2021-06-12 17:47:08 +0200

    Improve Payment page

--- src/pages/Payment/Payment.jsx
+++ src/pages/Payment/Payment.jsx
@@ -2,28 +2,10 @@ import React from "react";
 import { Link, useHistory } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
-import { Container } from "../../components/Container";
-
 import styles from "./style.scss";
 import { useSubscriptionFlow } from "../../utilities/subscription-flow";
-
-const InputComponent = (
-  { onChange, onBlur, name, label, type = "text" },
-  ref
-) => (
-  <>
-    <label>{label}</label>
-    <input
-      type={type}
-      onChange={onChange}
-      onBlur={onBlur}
-      name={name}
-      ref={ref}
-    />
-  </>
-);
-
-const Input = React.forwardRef(InputComponent);
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
 
 export function Payment() {
   // State to use data and set data to the Context component
@@ -39,49 +21,62 @@ export function Payment() {
 
   //Function to store data on the Context Component
   const onSubmit = (newPayment) => {
-    console.log(newPayment);
     setPayment(newPayment);
     history.push("/confirmation");
   };
 
   return (
-    <Container>
-      <div className={styles.Payment_Wrapper}>
-        <Link to="/">
-          <button>Subscription Plans</button>
-        </Link>
-        <form onSubmit={handleSubmit(onSubmit)}>
+    <div className={styles.Wrapper}>
+      <form onSubmit={handleSubmit(onSubmit)} className={styles.Form}>
+        <div className={styles.InnerWrapper}>
           <Input
             {...register("cardNumber", {
               required: true,
               value: subscriptionFlowState.payment?.cardNumber,
             })}
-            label="Card Number"
-            type="number"
+            label="Credit card number"
+            placeholder="Credit card number"
+            errorMessage={
+              errors.cardNumber?.type === "required" &&
+              "Card number is required"
+            }
           />
-          {errors.cardNumber?.type === "required" && "Card number is required"}
-          <Input
-            label="Expire Date"
-            {...register("cardExpirationDate", {
-              required: true,
-              pattern: /^(0[1-9]|1[0-2])\/?([0-9]{2})$/,
-              value: subscriptionFlowState.payment?.cardExpirationDate,
-            })}
-          />
-          {errors.cardExpirationDate?.type === "required" &&
-            "Card Expire Date is required"}
-          {errors.cardExpirationDate?.type === "pattern" &&
-            "Card Expire Date format MM/YY"}
-          <Input
-            label="Security Code"
-            type="number"
-            {...register("cardSecurityCode", { required: true })}
-          />
-          {errors.cardSecurityCode?.type === "required" &&
-            "Card Security Code is required"}
-          <input type="submit" />
-        </form>
-      </div>
-    </Container>
+          <div className={styles.ExpCodeWrapper}>
+            <Input
+              {...register("cardExpirationDate", {
+                required: true,
+                pattern: /^(0[1-9]|1[0-2])\/?([0-9]{2})$/,
+                value: subscriptionFlowState.payment?.cardExpirationDate,
+              })}
+              label="Expiration date"
+              placeholder="Expiration date"
+              errorMessage={
+                (errors.cardExpirationDate?.type === "required" &&
+                  "Card Expire Date is required") ||
+                (errors.cardExpirationDate?.type === "pattern" &&
+                  "Card Expire Date format MM/YY")
+              }
+            />
+            <Input
+              type="number"
+              {...register("cardSecurityCode", { required: true })}
+              label="Security code"
+              placeholder="Security code"
+              errorMessage={
+                errors.cardSecurityCode?.type === "required" &&
+                "Card Security Code is required"
+              }
+            />
+          </div>
+        </div>
+
+        <div className={styles.Buttons}>
+          <Link to="/">
+            <Button>Back to subscription plans</Button>
+          </Link>
+          <Button type={"submit"}>Confirm payment</Button>
+        </div>
+      </form>
+    </div>
   );
 }
